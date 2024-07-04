c24d50cbecd5bba3d0d828507d70a1cf33ed8b69
Aarsh Patel <aarsh18patel@gmail.com>
2024-07-04 17:54:26 +0530

    Initial Commit

--- index.js
+++ index.js
@@ -3,5 +3,10 @@ import qr from "qr-image"
 import fs from "fs"
 
 const answer = await input({ message: 'Enter the website URL: ' });
-const qr_png = qr.image(answer, { type: 'png' });
+const qr_png = qr.image(answer);
 qr_png.pipe(fs.createWriteStream('website_url.png'));
+
+fs.writeFile('user_input.txt', answer, 'utf8', (err) => {
+    if (err) throw err;
+    console.log('The file has been saved!');
+}); 
