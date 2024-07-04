6d451106a791cb204317ec6a0be9e806819acb92
Aarsh Patel <aarsh18patel@gmail.com>
2024-07-04 16:58:59 +0530

    Initial Commit

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ import fs from "fs"
 
 const answer = await input({ message: 'Enter the website URL: ' });
 const qr_png = qr.image(answer, { type: 'png' });
-qr_png.pipe(fs.createWriteStream('i_love_qr.png'));
+qr_png.pipe(fs.createWriteStream('website_url.png'));
